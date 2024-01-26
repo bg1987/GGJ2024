d8ea5dbc30aa5d5a3f445503e8cfa86cd6fee665
tjmichael <tj@tjmichael.dev>
2024-01-26 22:19:58 +0200

    BG

---
+++ Assets/BG.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG : MonoBehaviour
+{
+    [SerializeField] private Transform parent;
+    [SerializeField] [Range(0.0f,1.0f)] private float movementScale = 0.01f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        var position = transform.position;
+        var newX =  -1f * movementScale * position.x;
+        transform.position = new Vector3(newX, position.y, position.z);
+    }
+}
