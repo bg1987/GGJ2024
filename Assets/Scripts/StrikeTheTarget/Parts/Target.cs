2d5424fc4fb98d869f54482ae358d4c9c5e3d43e
tjmichael <tj@tjmichael.dev>
2024-01-26 16:14:46 +0200

    random target

--- Assets/Scripts/StrikeTheTarget/Parts/Target.cs
+++ Assets/Scripts/StrikeTheTarget/Parts/Target.cs
@@ -1,10 +1,23 @@
 using System;
 using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace StrikeTheTarget.Parts
 {
     public class Target : MonoBehaviour
     {
+        [SerializeField] private Transform lBumper;
+        [SerializeField] private Transform rBumper;
+
+        private void OnEnable()
+        {
+            var lBumperPosition = lBumper.position;
+            var delta = rBumper.position - lBumperPosition;
+            var normalized = delta.normalized;
+            var magnitude = delta.magnitude * Random.Range(0.25f,0.75f);
+
+            transform.SetPositionAndRotation(lBumperPosition + (normalized * magnitude), transform.rotation);
+        }
     }
 }
