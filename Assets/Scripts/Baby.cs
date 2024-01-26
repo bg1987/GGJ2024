763ef9a5f016167212556d160a27bfad8208df5b
Benny Gafanovich <benny@moonactive.com>
2024-01-26 10:16:26 +0200

    working core mechanic (wants and rapid tap)

--- Assets/Scripts/Baby.cs
+++ Assets/Scripts/Baby.cs
@@ -1,21 +1,97 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Baby : MonoBehaviour
 {
-    public int HP = 32;
+    public SpriteRenderer mySprite;
+    public Color happyColor;
+    public Color sadColor;
 
+    public float hpDecay = 6.6f; //about 15 seconds of idling
+    public float HP = 100;
+    
+    //every X seconds the wants will change.
+    public float wantChangeFrequency = 5;
+    
+    public AnimationCurve changeFrequency;
+    
+    public float wrongWantFactor = 2;
+    
+    
+    private float MaxHp;
+    private List<Station> possibleWants = new List<Station>();
+    private int currentWantIndex;
+    private float wantsTimer;
+    private Station currentWant;
+    
     private void Awake()
     {
         IOC.Register(this);
     }
 
-    // Start is called before the first frame update
     void Start()
     {
+        MaxHp = HP;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        HP -= hpDecay * Time.deltaTime;
+        mySprite.color = Color.Lerp(sadColor, happyColor, HP / MaxHp);
+        ManageWants();
+        if (HP < 0)
+        {
+            QuitGame();
+        }
+    }
+
+    private void ManageWants()
+    {
+        if (wantsTimer < 0)
+        {
+            PickNewWant();
+            Debug.Log("Baby wants "+ currentWant.gameObject.name);
+            wantsTimer = changeFrequency.Evaluate(Time.time);
+        }
+        else
+        {
+            wantsTimer -= Time.deltaTime;
+        }
+    }
+
+    private void PickNewWant()
+    {
+        var oldWant = currentWant; 
+        currentWantIndex = Random.Range(0, possibleWants.Count);
+        currentWant = possibleWants[currentWantIndex];
+        possibleWants.RemoveAt(currentWantIndex);
+        possibleWants.Add(oldWant);
+    }
+
+    public void AddWant(Station s)
+    {
+        possibleWants.Add(s);
+    }
+
+    public void AddHp(Station s, float toAdd)
+    {
+        if (currentWant != s)
+        {
+            toAdd = -wrongWantFactor * toAdd;
+        }
+        
+        if (HP < MaxHp)
+        {
+            HP += toAdd;
+        }
+    }
+    
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -1,22 +1,38 @@
+using System;
 using UnityEngine;
 
 public class Station : MonoBehaviour
 {
     public Color stationColor;
-
+    public float HPPerHit = 10f; 
+    
+    
     private Parent parent;
-
+    private Baby baby;
+    private bool isVisited;
+    
+    
     private void Start()
     {
         parent = IOC.Get<Parent>();
+        baby = IOC.Get<Baby>();
+        baby.AddWant(this);
+    }
+
+    private void Update()
+    {
+        if (isVisited && Input.GetKeyDown(KeyCode.Space))
+        {
+            baby.AddHp(this, HPPerHit);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other);
         if (other.gameObject.tag == "Player")
         {
             parent.ChangeColor(stationColor);
+            isVisited = true;
         }
     }
 
@@ -25,6 +41,7 @@ public class Station : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             parent.ResetColor();
+            isVisited = false;
         }
     }
 }
