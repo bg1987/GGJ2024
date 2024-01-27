7ec05bae270911a54411dcb8d02f6675350a3aca
Benny Gafanovich <benny@moonactive.com>
2024-01-27 12:15:55 +0200

    rattle rattle

--- Assets/Scripts/StrikeTheTarget/StrikerStation.cs
+++ Assets/Scripts/StrikeTheTarget/StrikerStation.cs
@@ -5,6 +5,7 @@ namespace StrikeTheTarget
     public class StrikerStation : StationBase
     {
         [SerializeField] private Transform plane;
+        [SerializeField] private Animator anim;
 
         protected override void Start()
         {
@@ -14,10 +15,17 @@ namespace StrikeTheTarget
 
         void Update()
         {
+            anim.SetBool("Visited", isVisited);
             if (isVisited != plane.gameObject.activeSelf)
             {
                 plane.gameObject.SetActive(isVisited);
             }
+        }
+
+        protected virtual void OnTriggerEnter2D(Collider2D other)
+        {
+            base.OnTriggerEnter2D(other);
+            parent.ChangeColor(Color.clear);
         }
 
         public void OnStrike(bool success)
@@ -25,6 +33,11 @@ namespace StrikeTheTarget
             if (isVisited && success)
             {
                 baby.AddHp(this, HPPerHit);
+            }
+
+            if (isVisited)
+            {
+                anim.SetTrigger("Action");
             }
         }
     }
